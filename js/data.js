31210f2b42a656c0cfe8a508065603a810a1f3a4
Wonseop Lee <wonseop.s.lee@gmail.com>
2020-12-29 08:30:13 +0000

    dynamically display entries, updated fetching correct data from API, checked all functionalities

--- js/data.js
+++ js/data.js
@@ -1,34 +1,62 @@
 /* exported data */
 
-var data = {
-  entries: 0,
-  profile: []
-};
+var data = [];
 
 var xhr = new XMLHttpRequest();
 var apiLink = '';
 var obj = {};
 
+
 function getVehicleInfo(vin, year) {
-  apiLink = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/'
-    + vin + '?format=json&modelyear=' + year;
+  apiLink = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/' + vin
+          + '?format=json&modelyear=' + year;
   xhr.open('GET', apiLink);
   xhr.responseType = 'json';
+
   xhr.addEventListener('load', function () {
+    let city = '';
+    let state = '';
+    let country = '';
+
     let result = xhr.response;
-    console.log('response:', result);
+    // console.log('response:', result);
     obj.vin = vin;
-    obj.make = result.Results[6].Value;
-    obj.model = result.Results[12].Value;
     obj.year = year;
-    obj.factory = result.Results[10].Value + ', ' + result.Results[14].Value;
-    obj.displacement = result.Results[71].Value;
-    obj.hp = result.Results[82].Value;
-    data.profile.push(obj);
-    data.entries++;
+    obj.photo = $addImageUrl.value;
+
+    for (let i = 0; i < result.Results.length; i++) {
+      if (result.Results[i].VariableId === 26) {
+        obj.make = result.Results[i].Value;
+      };
+
+      if (result.Results[i].VariableId === 28) {
+        obj.model = result.Results[i].Value;
+      };
+
+      if (result.Results[i].VariableId === 11) {
+        obj.displacement = result.Results[i].Value;
+      };
+
+      if (result.Results[i].VariableId === 71) {
+        obj.hp = result.Results[i].Value;
+      };
+
+      if (result.Results[i].VariableId === 31) {
+        city = result.Results[i].Value;
+      };
+
+      if (result.Results[i].VariableId === 77 && result.Results[i].Value !== null) {
+        state = (', ' + result.Results[i].Value);
+      };
+
+      if (result.Results[i].VariableId === 75) {
+        country = (', ' + result.Results[i].Value);
+      };
+    };
+    obj.factory = city + state + country;
+    data.push(obj);
   });
   xhr.send();
-  console.log(data);
 };
 
 window.addEventListener('beforeunload', function(e) {
--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ const $addForm = document.querySelector('#add-form');
 const $img = document.querySelector('img');
 const $addImageUrl = document.querySelector('#add-image-url');
 const $addVinInput = document.querySelector('#add-vin-input');
+const $addYearInput = document.querySelector('#add-year-input');
 const $addCancelBtn = document.querySelector('#add-cancel-btn');
 
 const $views = document.querySelectorAll('main > div');
+
+const $profiles = document.querySelector('.profiles');
 
 function swapView(dataView) {
   for (let i = 0; i < $views.length; i++) {
@@ -21,7 +24,7 @@ function swapView(dataView) {
 
 $viewForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  // swapView('view');
+  console.log('This function will swap view to profile view in issue 3');
 });
 
 $addProfileBtn.addEventListener('click', function (e) {
@@ -30,8 +33,8 @@ $addProfileBtn.addEventListener('click', function (e) {
 
 $addForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  getVehicleInfo($addVinInput.value, '2019');
-  // swapView('view');
+  getVehicleInfo($addVinInput.value, $addYearInput.value);
+  swapView('view');
 });
 
 $addCancelBtn.addEventListener('click', function(e) {
@@ -42,8 +45,24 @@ $addImageUrl.addEventListener('input', function(e) {
   $img.setAttribute('src', $addImageUrl.value);
 });
 
+for (let i = 0; i < data.length; i++) {
+  let $entryDiv = document.createElement('div');
+  let $input = document.createElement('input');
+  let $label = document.createElement('label');
 
-// var form = document.createElement('form');
-// var profilesDiv = document.createElement('div');
+  $entryDiv.setAttribute('class', 'entry');
+  $input.setAttribute('type', 'radio');
+  $input.setAttribute('id', 'entry' + i);
+  $input.setAttribute('name', 'profile');
+  $label.setAttribute('for', 'entry' + i);
+  $label.textContent = data[i].year + ' ' + data[i].make + ' ' + data[i].model  + ' ' + data[i].vin;
+
+  $entryDiv.append($input, $label);
+  $profiles.append($entryDiv);
+}
+
 // WBXHU7C50K3H44275
 // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb096ibPU3KBSeNWHnDujBzWakpj5O9hlXdrYbpwgh2_qaqUjQ
+
+// 19XFC2F50GE234074
+// https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS0ffAuLvtMqmlj8s7UiroOuJYAoC-zj_5fG_1kBNSkwPpPUD5c
