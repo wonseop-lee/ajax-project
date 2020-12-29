be9a57fb772c6d95b4d2d3d08886d1ee4adf94c0
Wonseop Lee <wonseop.s.lee@gmail.com>
2020-12-29 00:00:57 +0000

    cleaned some html and css, confirmed the localstorage function is working, preliminary view swap function in place, preliminary storing of JSON from API working as intended

--- js/data.js
+++ js/data.js
@@ -2,19 +2,12 @@
 
 var data = {
   entries: 0,
-  profile: [{
-    vin: '',
-    make: '',
-    model: '',
-    year: '',
-    trim: '',
-    displacement: '',
-    hp: ''
-  }]
+  profile: []
 };
 
 var xhr = new XMLHttpRequest();
 var apiLink = '';
+var obj = {};
 
 function getVehicleInfo(vin, year) {
   apiLink = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/'
@@ -22,9 +15,20 @@ function getVehicleInfo(vin, year) {
   xhr.open('GET', apiLink);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    console.log('response:', xhr.response);
+    let result = xhr.response;
+    console.log('response:', result);
+    obj.vin = vin;
+    obj.make = result.Results[6].Value;
+    obj.model = result.Results[12].Value;
+    obj.year = year;
+    obj.factory = result.Results[10].Value + ', ' + result.Results[14].Value;
+    obj.displacement = result.Results[71].Value;
+    obj.hp = result.Results[82].Value;
+    data.profile.push(obj);
+    data.entries++;
   });
   xhr.send();
+  console.log(data);
 };
 
 window.addEventListener('beforeunload', function(e) {
--- js/main.js
+++ js/main.js
@@ -1,39 +1,49 @@
 const $viewForm = document.querySelector('#view-form');
 const $addProfileBtn = document.querySelector('#add-prof-btn');
-const $addForm = document.querySelector('#add-form');
-const $cancelBtn = document.querySelector('#cancel-btn');
-const $addImage = document.querySelector('#add-image');
-const $img = document.querySelector('img');
 
-// var $main = document.querySelector('main');
+const $addForm = document.querySelector('#add-form');
+const $img = document.querySelector('img');
+const $addImageUrl = document.querySelector('#add-image-url');
+const $addVinInput = document.querySelector('#add-vin-input');
+const $addCancelBtn = document.querySelector('#add-cancel-btn');
+
+const $views = document.querySelectorAll('main > div');
 
 function swapView(dataView) {
-  // issue 2
-}
+  for (let i = 0; i < $views.length; i++) {
+    if ($views[i].dataset.view === dataView) {
+      $views[i].setAttribute('class', 'shown');
+    } else {
+      $views[i].setAttribute('class', 'hidden');
+    }
+  }
+};
 
 $viewForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  // swapView();
+  // swapView('view');
 });
 
 $addProfileBtn.addEventListener('click', function (e) {
-  // swapView();
+  swapView('add');
 });
 
 $addForm.addEventListener('submit', function(e) {
   e.preventDefault();
+  getVehicleInfo($addVinInput.value, '2019');
+  // swapView('view');
 });
 
-$cancelBtn.addEventListener('click', function(e) {
-
+$addCancelBtn.addEventListener('click', function(e) {
+  swapView('profiles');
 });
 
-$addImage.addEventListener('keyup', function(e) {
-  $img.setAttribute('src', $addImage.value);
+$addImageUrl.addEventListener('input', function(e) {
+  $img.setAttribute('src', $addImageUrl.value);
 });
 
 
-
-
-var form = document.createElement('form');
-var profilesDiv = document.createElement('div');
+// var form = document.createElement('form');
+// var profilesDiv = document.createElement('div');
+// WBXHU7C50K3H44275
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb096ibPU3KBSeNWHnDujBzWakpj5O9hlXdrYbpwgh2_qaqUjQ
